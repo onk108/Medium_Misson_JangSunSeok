f62758a7ed3306e77af3fec9ce5c7aa2a16409d4
onk108 <wkdtnstjr88@naver.com>
2023-12-08 06:50:47 +0900

    medium

--- src/main/java/com/ll/medium/Answer.java
+++ src/main/java/com/ll/medium/Answer.java
@@ -1,11 +1,9 @@
 package com.ll.medium;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.OneToMany;
 
 import jakarta.persistence.*;
+import jakarta.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,20 +11,17 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class Question {
+public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(length = 200)
-    private String subject;
 
     @Column(columnDefinition = "TEXT")
     private String content;
 
     private LocalDateTime createDate;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> answerList;
+    @ManyToOne
+    private Question question;
 }
 
