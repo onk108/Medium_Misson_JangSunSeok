477b2214b63afd28437e39cc178ac420d96030c8
onk108 <wkdtnstjr88@naver.com>
2023-12-07 10:56:32 +0900

    medium

--- src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
@@ -1,11 +1,20 @@
 package com.ll.medium.domain.article.article.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ll.medium.domain.article.article.entity.Article;
 import com.ll.medium.domain.article.article.service.ArticleService;
+import com.ll.medium.global.rq.Rq;
 import com.ll.medium.global.rsData.RsData;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,21 +24,26 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 public class ArticleController {
-
     private final ArticleService articleService;
+    private final Rq rq;
 
     @GetMapping("/article/write")
     String showWrite() {
         return "article/write";
     }
 
+    @Data
+    public static class WriteForm {
+        @NotBlank
+        private String title;
+        @NotBlank
+        private String body;
+    }
+
     @PostMapping("/article/write")
     @ResponseBody
-    RsData write(
-            String title,
-            String body
-    ) {
-        Article article = articleService.write(title, body);
+    RsData write(@Valid WriteForm writeForm) {
+        Article article = articleService.write(writeForm.title, writeForm.body);
 
         RsData<Article> rs = new RsData<>(
                 "S-1",
@@ -38,6 +52,11 @@ public class ArticleController {
         );
 
         return rs;
+    }
+
+    @GetMapping("/article/list")
+    String showList() {
+        return "article/list";
     }
 
     @GetMapping("/article/getLastArticle")
@@ -50,5 +69,38 @@ public class ArticleController {
     @ResponseBody
     List<Article> getArticles() {
         return articleService.findAll();
+    }
+
+    @GetMapping("/article/articleServicePointer")
+    @ResponseBody
+    String articleServicePointer() {
+        return articleService.toString();
+    }
+
+    @GetMapping("/article/httpServletRequestPointer")
+    @ResponseBody
+    String httpServletRequestPointer(HttpServletRequest req) {
+        return req.toString();
+    }
+
+    @GetMapping("/article/httpServletResponsePointer")
+    @ResponseBody
+    String httpServletResponsePointer(HttpServletResponse resp) {
+        return resp.toString();
+    }
+
+    @GetMapping("/article/rqPointer")
+    @ResponseBody
+    String rqPointer() {
+        return rq.toString();
+    }
+
+    @GetMapping("/article/rqTest")
+    String showRqTest(Model model) {
+        String rqToString = rq.toString();
+
+        model.addAttribute("rqToString", rqToString);
+
+        return "article/rqTest";
     }
 }
