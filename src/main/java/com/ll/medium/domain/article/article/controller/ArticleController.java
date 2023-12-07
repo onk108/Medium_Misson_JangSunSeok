6dc60b7867b90e5c9a5c2173768700b611d6e1d0
onk108 <wkdtnstjr88@naver.com>
2023-12-07 11:47:14 +0900

    medium

--- src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
@@ -55,7 +55,11 @@ public class ArticleController {
     }
 
     @GetMapping("/article/list")
-    String showList() {
+    String showList(Model model) {
+        List<Article> articles = articleService.findAll();
+
+        model.addAttribute("articles", articles);
+
         return "article/list";
     }
 
