16fd9cb1ad8227db78e2b806da0a14a9983c3178
onk108 <wkdtnstjr88@naver.com>
2023-12-08 17:39:52 +0900

    medium

---
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -0,0 +1,72 @@
+package com.ll.medium.domain.member.member.controller;
+
+import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.member.member.service.MemberService;
+import com.ll.medium.global.rq.Rq;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+    private final Rq rq;
+
+    @GetMapping("/member/login")
+    String showLogin() {
+        return "member/member/login";
+    }
+
+    @Data
+    public static class LoginForm {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
+    @PostMapping("/member/login")
+    String login(@Valid LoginForm loginForm, HttpServletResponse response) {
+        Member member = memberService.findByUsername(loginForm.username).get();
+
+        if (!member.getPassword().equals(loginForm.password)) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // 로그인 처리
+        Cookie cookie = new Cookie("loginedMemberId", "2");
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return rq.redirect("/article/list", "로그인이 완료되었습니다.");
+    }
+
+    @GetMapping("/member/join")
+    String showJoin() {
+        return "member/member/join";
+    }
+
+    @Data
+    public static class JoinForm {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
+    @PostMapping("/member/join")
+    String join(@Valid JoinForm joinForm) {
+        memberService.join(joinForm.username, joinForm.password);
+
+        return rq.redirect("/member/login", "회원가입이 완료되었습니다.");
+    }
+}
---
+++ src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
@@ -0,0 +1,46 @@
+package com.ll.medium.domain.member.member.service;
+
+import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.member.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member join(String username, String password) {
+        Member member = new Member(username, password);
+
+        memberRepository.save(member);
+
+        return member;
+    }
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findById(long id) {
+        return memberRepository.findById(id);
+    }
+
+    public Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    public void delete(long id) {
+        memberRepository.delete(id);
+    }
+
+    public void modify(long id, String username, String password) {
+        Member member = findById(id).get();
+        member.setUsername(username);
+        member.setPassword(password);
+    }
+}
