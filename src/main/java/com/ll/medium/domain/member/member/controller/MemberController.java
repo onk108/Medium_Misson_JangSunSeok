5ff61a75d202866b0c6d2a0732ad08a0e1e0fe16
onk108 <71273335+onk108@users.noreply.github.com>
2024-02-06 23:41:12 +0900

    124

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -68,16 +68,8 @@ public class MemberController {
     @PreAuthorize("isAnonymous()")
     @PostMapping("/member/join")
     String join(@Valid JoinForm joinForm) {
-        try {
-            RsData<Member> joinRs = memberService.join(joinForm.username, joinForm.password);
+        RsData<Member> joinRs = memberService.join(joinForm.username, joinForm.password);
 
-            if(joinRs.isFail()) {
-                return rq.historyBack(joinRs.getMsg());
-            }
-
-            return rq.redirect("/member/login", joinRs.getMsg());
-        } catch (RuntimeException e) {
-            return rq.historyBack(e.getMessage());
-        }
+        return rq.redirect("/member/login", joinRs.getMsg());
     }
 }
